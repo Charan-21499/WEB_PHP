bfd976ab92b3111ba2d2e72447b79cc539890f81
Charan-21499 <56217763+Charan-21499@users.noreply.github.com>
2020-02-22 23:20:55 -0500

    Add files via upload

---
+++ PHP/try/one.php
@@ -0,0 +1,26 @@
+
+<link rel="stylesheet" type="text/css" href="one.css">
+<div class="page-content page-container" id="page-content">
+    <div class="padding">
+        <div class="row container d-flex justify-content-center">
+            <div class="col-lg-8 grid-margin stretch-card">
+                <!--weather card-->
+                <div class="card card-weather">
+                    <div class="card-body">
+                        <div class="weather-date-location">
+                            <h3>IMAGE FROM THE API</h3>
+                                <h2 class="text-gray"> <span class="weather-date">DATE MONTH, YEAR</span> <span class="weather-location">CITY, COUNTRY</span> </h2>
+                        </div>
+                        <div class="weather-data d-flex">
+                            <div class="mr-auto">
+                                <h4 class="display-3">TEMPERATURE<span class="symbol">°</span>F</h4>
+                                <h4>WEATHER </h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--weather card ends-->
+            </div>
+        </div>
+    </div>
+</div>
